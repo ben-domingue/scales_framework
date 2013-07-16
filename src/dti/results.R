7b5e1ce251ba7ca3d2129fe2d987b760e58ea9ed
rdiakow <rdiakow@berkeley.edu>
2013-07-16 09:49:50 -0400

    latest results graphs

--- src/dti/results.R
+++ src/dti/results.R
@@ -3,17 +3,21 @@
 setwd("C:/Users/Ronli/Documents/GitHub/scales_framework/src/dti")
 
 ##read in simulation results
-load("./simdata/scale_cols.Rdata")  #obj called out_cols
-load("./simdata/scale_rows.Rdata")  #obj called out_rows
+load("./simdata3/scale_cols.Rdata")  #obj called out_cols
+load("./simdata3/scale_rows.Rdata")  #obj called out_rows
+
+##reorder
+out_cols_ord <- out_cols[mixedsort(names(out_cols))]
+out_rows_ord <- out_rows[mixedsort(names(out_rows))]
 
 ##pull out proportion of violations
-violations.cols <- data.frame(model = ordered(rep(c("UNC","MON","IIO","DM","LCR"), each = 50), levels = c("UNC","MON","IIO","DM","LCR")), 
-                              unwei = sapply(out_cols, function(x) x@means$unweighted), 
-                              weigh = sapply(out_cols, function(x) x@means$weighted))
+violations.cols <- data.frame(model = ordered(rep(c("UNC","MON","IIO","DM","LCR","RSH"), each = 50), levels = c("UNC","MON","IIO","DM","LCR","RSH")), 
+                              unwei = sapply(out_cols_ord, function(x) x@means$unweighted), 
+                              weigh = sapply(out_cols_ord, function(x) x@means$weighted))
 
-violations.rows <- data.frame(model = ordered(rep(c("UNC","MON","IIO","DM","LCR"), each = 50), levels = c("UNC","MON","IIO","DM","LCR")), 
-                              unwei = sapply(out_rows, function(x) x@means$unweighted), 
-                              weigh = sapply(out_rows, function(x) x@means$weighted))
+violations.rows <- data.frame(model = ordered(rep(c("UNC","MON","IIO","DM","LCR","RSH"), each = 50), levels = c("UNC","MON","IIO","DM","LCR","RSH")), 
+                              unwei = sapply(out_rows_ord, function(x) x@means$unweighted), 
+                              weigh = sapply(out_rows_ord, function(x) x@means$weighted))
 
 ##plot results
 boxplot(weigh ~ model, violations.cols, ylim = c(0,0.4))
